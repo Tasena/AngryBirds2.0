69af5cc46b0adde4a4bb69c9a4829372fbdf9cb4
Tasena <kusumtasena@gmail.com>
2020-07-18 16:48:19 +0530

    box12 added

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1,box12;
 var backgroundImg,platform;
 
 function preload() {
@@ -18,6 +18,7 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
+    box12 = new Box(20,320,70,70);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
